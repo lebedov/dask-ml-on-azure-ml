4ca96824157608ae4ab2e9e58f45494b2c6ec4f5
Lev E. Givon <lev@columbia.edu>
2019-11-20 14:12:01 -0500

    Add support for not running cross val, show how to use either ScriptRunConfig or Estimator.

--- run.py
+++ run.py
@@ -2,11 +2,12 @@
 
 import json
 
-from azureml.core import Experiment, Workspace
+from azureml.core import Experiment, Workspace, ScriptRunConfig
 from azureml.core.authentication import InteractiveLoginAuthentication
 from azureml.core.compute import ComputeTarget, AmlCompute
 from azureml.core.compute_target import ComputeTargetException
-from azureml.core.runconfig import MpiConfiguration
+from azureml.core.conda_dependencies import CondaDependencies
+from azureml.core.runconfig import MpiConfiguration, RunConfiguration, DEFAULT_CPU_IMAGE, DEFAULT_GPU_IMAGE
 from azureml.train.estimator import Estimator
 
 # Load tenant ID from config.json; the tenant ID must be manually obtained 
@@ -17,11 +18,17 @@ interactive_auth = InteractiveLoginAuthentication(tenant_id=tenant_id)
 
 # This will open a web page to enable one to authenticate:
 ws = Workspace.from_config(auth=interactive_auth)
-mpi_conf = MpiConfiguration()
+run_conf = RunConfiguration()
+run_conf.framework = 'Python'
+
+# Number of folds for cross validation; if set to None, no cross validation is
+# performed (and hence dask is not used):
+cv = 3
 
 # Use local development environment:
 compute_name = config['compute_name']
 if compute_name == 'local':
+    run_conf.environment.python.user_managed_dependencies = True
     compute_target = compute_name
 
 # Use AzureML compute target:
@@ -37,16 +44,42 @@ else:
         compute_target = ComputeTarget.create(ws, compute_name, compute_config)
         compute_target.wait_for_completion(show_output=True)
 
+    run_conf.target = compute_target
+    run_conf.environment.docker.enabled = True
+    run_conf.environment.docker.base_image = DEFAULT_CPU_IMAGE
+    run_conf.environment.python.conda_dependencies = \
+        CondaDependencies(conda_dependencies_file_path='env.yml')
+    run_conf.environment.python.user_managed_dependencies = False
+    if cv:
+        run_conf.communicator = 'OpenMPI'
+        run_conf.mpi = MpiConfiguration()
+        run_conf.node_count = cv+2
 exp = Experiment(workspace=ws, name=config['experiment_name'])
 
-cv = 3
-to_run = Estimator(source_directory='.',
-                   compute_target=compute_target,
-                   entry_script='train.py',
-                   script_params={'--cv': cv},
-                   node_count=cv+2, # dask-mpi uses 2 nodes for its scheduler and client
-                   conda_dependencies_file='env.yml',
-                   distributed_training=mpi_conf)
-
+use_estimator = True
+if use_estimator:
+    if cv:
+        script_params = {'--cv': cv}
+        node_count = cv+2 # dask-mpi uses 2 nodes for its scheduler and client 
+    else:
+        script_params = None
+        node_count = None
+    to_run = Estimator(source_directory='.',
+                       compute_target=compute_target,
+                       entry_script='train.py',
+                       script_params=script_params,
+                       node_count=node_count,
+                       use_gpu=False,
+                       conda_dependencies_file='env.yml',
+                       distributed_training=MpiConfiguration())
+else:
+    if cv:
+        arguments = ['--cv', str(cv)]
+    else:
+        arguments = []
+    to_run = ScriptRunConfig(source_directory='.',
+                             script='train.py',
+                             arguments=arguments,
+                             run_config=run_conf)
 run = exp.submit(to_run)
 run.wait_for_completion(show_output=True)
--- train.py
+++ train.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
 
-from dask_mpi import initialize
-initialize()
-
-from distributed import Client
-client = Client()
-
 import argparse
-
-from azureml.core import Run
-
-import numpy as np
-from sklearn.datasets import load_digits
-from dask_ml.model_selection import GridSearchCV
-from sklearn.svm import SVC
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--cv', nargs='?', const=3, type=int,
                     help='do cross validation with specified number of folds')
 args = parser.parse_args()
 
+# Only initialize dask-mpi and dask if performing cross validation:
+if args.cv:
+    from dask_mpi import initialize
+    initialize()
+
+    from distributed import Client
+    client = Client()
+
+    from dask_ml.model_selection import GridSearchCV, train_test_split
+else:
+    from sklearn.model_selection import GridSearchCV, train_test_split
+
+from azureml.core import Run
+import numpy as np
+from sklearn.datasets import load_digits
+import sklearn.metrics as skmetrics
+from sklearn.svm import SVC
+
 run = Run.get_context()
+
 # For a bigger dataset, try
 # x, y = make_classification(5000, n_classes=16, n_informative=13)
 digits = load_digits()
@@ -33,8 +39,14 @@ param_space = {
 }
 
 model = SVC(kernel='rbf')
-search = GridSearchCV(model, param_space, scoring=['accuracy'],
-                      refit=False, cv=args.cv)
-search.fit(x, y)
-run.log('accuracy_mean', search.cv_results_['mean_test_accuracy'][0])
-run.log('accuracy_std', search.cv_results_['std_test_accuracy'][0])
+if args.cv:
+    search = GridSearchCV(model, param_space, scoring=['accuracy'],
+                          refit=False, cv=args.cv)
+    search.fit(x, y)
+    run.log('accuracy_mean', search.cv_results_['mean_test_accuracy'][0])
+    run.log('accuracy_std', search.cv_results_['std_test_accuracy'][0])
+else:
+    x_train, x_test, y_train, y_test = train_test_split(x, y)
+    model.fit(x_train, y_train)
+    y_test_pred = model.predict(x_test)
+    run.log('accuracy', skmetrics.accuracy_score(y_test, y_test_pred))
